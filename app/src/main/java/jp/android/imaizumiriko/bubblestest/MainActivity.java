6569b1fdc04ab2c650e03d5cbbb1c7f252b0ddab
yukiyuki961 <yukinabe3561@icloud.com>
2019-07-17 14:39:54 +0900

    Fix Error

--- app/src/main/java/jp/android/imaizumiriko/bubblestest/MainActivity.java
+++ app/src/main/java/jp/android/imaizumiriko/bubblestest/MainActivity.java
@@ -4,58 +4,58 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int OVERLAY_PERMISSION_REQ_CODE = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // API 23 以上であればPermission chekを行う
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        checkPermission();
+        //}
 
-        //API 23 以上であればpermission checkを行う
-        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M {
-          checkPermission();
+        // Serviceを開始するためのボタン
+        Button buttonStart = findViewById(R.id.button_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), TestService.class);
+                // Serviceの開始
+                // API26以上
+                startForegroundService(intent);
+            }
+        });
     }
 
-    //Serviceを開始するためのボタン
-    Button buttonStart = findViewById(R.id.button_start);
-    buttonStart.setOnclickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(getApplication(), TestService.class);
-            //Serviceの開始
-            //API26以上
-            startForegroundService(intent);
-
+    @TargetApi(Build.VERSION_CODES.M)
+    public void checkPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
         }
-    });
-}
-
-@TargetApi(Build.VERSION_CODES.M)
-public void checkPermission(){
-    if(!Settings.canDrawOverlays(this)) {
-        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                Uri.parse("package:" + getPackageName()));
-        startActivityForResult(intent,OVERLAY_PERMISSION_REQ_CODE);
     }
 
-}
-
-@TargetApi(Build.VERSION_CODES.M)
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if(requestCode == OVERLAY_PERMISSION_REQ_CODE){
-        if(!Settings.canDrawOverlays(this)){
-            Log.d("debug","SYSTEM_ALERT_WINDOW permission not granted...");
-            // SYSTEM_ALERT_WINDOW permission not granted...
-            //nothing to do !
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                Log.d("debug", "SYSTEM_ALERT_WINDOW permission not granted...");
+                // SYSTEM_ALERT_WINDOW permission not granted...
+                // nothing to do !
+            }
         }
     }
 }
--- app/src/main/java/jp/android/imaizumiriko/bubblestest/TestService.java
+++ app/src/main/java/jp/android/imaizumiriko/bubblestest/TestService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
@@ -19,123 +20,124 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class Testservice extends AppCompatActivity {
+public class TestService extends Service {
 
     private View view;
     private WindowManager windowManager;
     private int dpScale ;
 
     @Override
-    protected void onCreate() {
+    public void onCreate() {
         super.onCreate();
-
 
         // dipを取得
         dpScale = (int)getResources().getDisplayMetrics().density;
     }
 
     @Override
-    public int onStartCommand(Intent intent,int flags,int startId){
+    public int onStartCommand(Intent intent, int flags, int startId) {
 
-        //startForegroundService()
+        // startForegroundService() -----
         Context context = getApplicationContext();
         String channelId = "default";
         String title = context.getString(R.string.app_name);
 
         PendingIntent pendingIntent =
-                PendingIntent.getActivity(context,0,intent,
+                PendingIntent.getActivity(context, 0, intent,
                         PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationManager notificationManager =
                 (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        // Notification　Channel 設定
         NotificationChannel channel = new NotificationChannel(
-                channelId,title,NotificationManager.IMPORTANCE_DEFAULT);
+                channelId, title , NotificationManager.IMPORTANCE_DEFAULT);
 
         if(notificationManager != null){
             notificationManager.createNotificationChannel(channel);
 
-            Notification notification = new Notification.Builder(context.channelId)
+            Notification notification = new Notification.Builder(context, channelId)
                     .setContentTitle(title)
-                    //android標準アイコンから
-            .setSmallIcon(android.R.drawable.btn_star)
-            .setContentText("APPLICATION_OVERLAY")
-            .setAutoCancel(true)
-            .setContentIntent(pendingIntent)
-            .setWhen(System.currentTimeMillis())
-            .build();
+                    // android標準アイコンから
+                    .setSmallIcon(android.R.drawable.btn_star)
+                    .setContentText("APPLICATION_OVERLAY")
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setWhen(System.currentTimeMillis())
+                    .build();
 
             // startForeground
             startForeground(1, notification);
 
-
         }
-        //----- startForegroundService()
+        // ----- startForegroundService()
+
 
         // inflaterの生成
         LayoutInflater layoutInflater = LayoutInflater.from(this);
 
         int typeLayer = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
 
+
         windowManager = (WindowManager)getApplicationContext()
                 .getSystemService(Context.WINDOW_SERVICE);
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams (
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 WindowManager.LayoutParams.WRAP_CONTENT,
                 typeLayer,
-                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                 PixelFormat.TRANSLUCENT);
 
-        //右上に配置
-        params.gravity= Gravity.TOP 1 Gravity.END;
-    params.x = 20 * dpScale; //20dp
-    params.y = 80 * dpScale;//80dp
+        // 右上に配置
+        params.gravity=  Gravity.TOP | Gravity.END;
+        params.x = 20 * dpScale; // 20dp
+        params.y = 80 * dpScale; // 80dp
 
-    //レイアウトファイルからInfalteするViewを作成
-    final ViewGroup nullParent = null;
-    view = layoutInflater.inflate(R.layout.service_layer,nullParent);
+        // レイアウトファイルからInfalteするViewを作成
+        final ViewGroup nullParent = null;
+        view = layoutInflater.inflate(R.layout.service_layer, nullParent);
 
-    //ViewにTouchListenerを設定する
+
+        // ViewにTouchListenerを設定する
         view.setOnTouchListener(new View.OnTouchListener(){
             @Override
-            public boolean onTouch(View v, MotionEvent event){
+            public boolean onTouch(View v, MotionEvent event) {
                 Log.d("debug","onTouch");
-                if(event.getAction() ==MotionEvent.ACTION_DOWN){
+                if(event.getAction() == MotionEvent.ACTION_DOWN){
                     Log.d("debug","ACTION_DOWN");
                 }
                 if(event.getAction() == MotionEvent.ACTION_UP){
                     Log.d("debug","ACTION_UP");
 
-                    //warning: override performClick()
+                    // warning: override performClick()
                     view.performClick();
 
-                    //Serviceを自ら停止させる
+                    // Serviceを自ら停止させる
                     stopSelf();
-
                 }
                 return false;
-
             }
         });
 
         // Viewを画面上に追加
-        windowManager.addView(view,params);
+        windowManager.addView(view, params);
 
-        return super.onStartCommand(intent,flags,startId);
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
         Log.d("debug","onDestroy");
-        //Viewを削除
+        // Viewを削除
         windowManager.removeView(view);
     }
 
     @Override
-    public IBinder onBind(Intent intent){
-        //TODO Auto-generated method stub
+    public IBinder onBind(Intent intent) {
+        // TODO Auto-generated method stub
         return null;
     }
 }
